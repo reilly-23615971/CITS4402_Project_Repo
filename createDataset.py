c3cf54e8fb4a38840ea5c2300b97bec142f780cd
reilly-23615971 <23615971@student.uwa.edu.au>
2025-04-21 17:07:17 +0800

    Converted code to function and improved formatting

--- createDataset.py
+++ createDataset.py
@@ -11,45 +11,104 @@ import shutil
 import tarfile
 import numpy as np
 
-# Constants
-trainSize = 500
-extractLocation = os.path.abspath('./Data/extractedData')
-testOutput = os.path.abspath('./Data/testData')
-trainOutput = os.path.abspath('./Data/trainData')
 
-# Load the Daimer dataset
-with tarfile.open("DC-ped-dataset_base.tar", "r") as tf:
-    tf.extract(member='1/non-ped_examples', path="./test")
-    for file in tf.getmembers():
-        if file.name.startswith('1/'): tf.extract(file, path=extractLocation)
+"""
+Function to extract images from a given tarfile and select random ones
+to form training and testing sets
 
-# TODO: Check if sampling from the tarfile without extracting the whole thing is possible
+Parameters:
+    tarfilePath: name of (or path to) the tarfile containing the images 
+    that will be used to make the training/testing sets
 
-# Select files for training and testing; use 50/50 split of pedestrian and non-pedestrian for training
-pedestrianDir = os.path.join(extractLocation, '1/ped_examples')
-nonPedestrianDir = os.path.join(extractLocation, '1/non-ped_examples')
-testPedestrian = [img for img in np.random.choice(os.listdir(pedestrianDir), 100, replace=False)]
-testNonPedestrian = [img for img in np.random.choice(os.listdir(nonPedestrianDir), 100, replace=False)]
-trainPedestrian = [img for img in np.random.choice(os.listdir(pedestrianDir), trainSize//2, replace=False)]
-trainNonPedestrian = [img for img in np.random.choice(os.listdir(nonPedestrianDir), trainSize//2, replace=False)]
+    trainSize: number of images to select for the training set
 
-# Copy selected images to their own directory
-# TODO: Check to ensure that like-numbered images from pedestrian and non-pedestrian folders don't override each other
-if not os.path.exists(testOutput):
-    os.makedirs(testOutput)
-if not os.path.exists(trainOutput):
-    os.makedirs(trainOutput)
-for img in testPedestrian:
-    imgPath = os.path.join(pedestrianDir, img)
-    shutil.copy(imgPath, testOutput)
-for img in testNonPedestrian:
-    imgPath = os.path.join(nonPedestrianDir, img)
-    shutil.copy(imgPath, testOutput)
-for img in trainPedestrian:
-    imgPath = os.path.join(pedestrianDir, img)
-    shutil.copy(imgPath, trainOutput)
-for img in trainNonPedestrian:
-    imgPath = os.path.join(nonPedestrianDir, img)
-    shutil.copy(imgPath, trainOutput)
+    imagePath: directory in tarfile to select images from, containing 
+    folders for positive and negative examples. Must end in '/' to allow
+    for extracting only the images from the tarfile
 
-# TODO: Compress extracted datasets
+    positiveSamples: folder in tarPath containing positive samples
+
+    negativeSamples: folder in tarPath containing negative samples
+
+    extractPath: directory to output the extracted tarfile images into
+
+    trainPath: directory where the images selected for the training set 
+    are outputted
+
+    testPath: directory where the images selected for the testing set 
+    are outputted
+
+    withReplacement: whether duplicate images are possible in the sets
+
+    randomState: NumPy seed for random selection reproducibility
+
+
+Default parameters are based on the Daimer dataset and the project 
+task statement where applicable
+Due to always selecting an equal number of positive and negative images,
+the number of images in the training dataset will be 1 lower than 
+trainSize when the parameter is odd
+The testing set will always have 100 positive and 100 negative images 
+per the project task statement
+"""
+def createDataset(
+        tarfilePath, trainSize = 500, 
+        extractPath = './Data/extractedData', imagePath = '1/', 
+        positiveSamples = 'ped_examples', negativeSamples = 'non-ped_examples',
+        trainPath = './Data/testData', testPath = './Data/trainData',
+        withReplacement = False, randomState = None):
+    # TODO: Validate parameters
+
+    # Create output directories if they don't exist already
+    for path in [extractPath, trainPath, testPath]:
+        if not os.path.exists(path): os.makedirs(path)
+
+    # Load the dataset
+    with tarfile.open(tarfilePath, 'r') as tf:
+        for file in tf.getmembers():
+            if file.name.startswith(imagePath): 
+                tf.extract(file, path=extractPath)
+    
+    # TODO: Check if sampling from the tarfile without extracting the whole thing is possible
+
+    # Get locations of newly extracted positive and negative samples
+    extractedPositive = os.path.join(extractPath, imagePath, positiveSamples)
+    extractedNegative = os.path.join(extractPath, imagePath, negativeSamples)
+
+    # Set random state for reproducibility
+    np.random.seed(randomState)
+
+    # Randomly select enough files for training and testing sets
+    sampledPositive = [img for img in np.random.choice(
+        os.listdir(extractedPositive), (trainSize//2) + 100, 
+        replace = withReplacement
+    )]
+    sampledNegative = [img for img in np.random.choice(
+        os.listdir(extractedNegative), (trainSize//2) + 100, 
+        replace = withReplacement
+    )]
+
+    # Split out training and testing images to ensure there's no overlap
+    testPositive, trainPositive = sampledPositive[:100], sampledPositive[100:]
+    testNegative, trainNegative = sampledNegative[:100], sampledNegative[100:]
+
+    # TODO: Add prefix to positive/negative images so that no identical image issues occur
+
+    # Copy selected images into their own directories
+    for img in trainPositive:
+        newPath = os.path.join(extractedPositive, img)
+        shutil.copy(newPath, trainPath)
+    for img in trainNegative:
+        newPath = os.path.join(extractedNegative, img)
+        shutil.copy(newPath, trainPath)
+    for img in testPositive:
+        newPath = os.path.join(extractedPositive, img)
+        shutil.copy(newPath, testPath)
+    for img in testNegative:
+        newPath = os.path.join(extractedNegative, img)
+        shutil.copy(newPath, testPath)
+    
+    # TODO: Compress extracted datasets 
+
+# Run createDataset
+createDataset('DC-ped-dataset_base.tar', randomState = 42)
