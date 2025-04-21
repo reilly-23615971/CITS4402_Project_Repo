0436b6c323a8a99f6cab8e02407a92c508144ade
reilly-23615971 <23615971@student.uwa.edu.au>
2025-04-21 21:36:03 +0800

    createDataset now compresses its generated sets, directories created during the set generating process are deleted afterwards, now only extracts the images it actually uses for the sets, simplified parameters and added new ones for the output tarfile

--- createDataset.py
+++ createDataset.py
@@ -12,82 +12,77 @@ import tarfile
 import numpy as np
 
 """
-Helper function to copy specified images to a new directory, renaming 
-them in the process to avoid duplicate name issues
+Function to extract images from a given tarfile, select a random sample
+to form training and testing sets, and recompress the new sets into 
+their own tarfiles
 
-Parameters:
-    images: list of image file names
-
-    baseDir: directory where images are currently stored
-
-    prefix: string to add to beginning of renamed files
-
-    compilePath: folder to copy the images to
-"""
-def copyImageList(images, basePath, prefix, compilePath):
-    for index, img in enumerate(images):
-        _, ext = os.path.splitext(img)
-        shutil.copy(
-            os.path.join(basePath, img), 
-            os.path.join(compilePath, (prefix + f'{index:05}' + ext))
-        )
-
-"""
-Function to extract images from a given tarfile and select random ones
-to form training and testing sets
 
 Parameters:
     tarfilePath: name of (or path to) the tarfile containing the images 
     that will be used to make the training/testing sets
 
-    trainSize: number of images to select for the training set
+    trainSize: number of images to select for the training set; defaults
+    to 500 since that is the minimum set by the project task statement
 
-    imagePath: directory in tarfile to extract and select images from, 
-    containing folders for positive and negative examples. Avoid 
-    trailing slashes for this variable, as if one is present the code to
-    select images from the tarfile may fail
+    workingPath: the temporary folder where the tarfile's extracted 
+    images for the training/testing sets will go while the function is 
+    executing. The function will raise an error if this directory 
+    already exists; this is by design so as to avoid overwriting 
+    existing folders.
 
-    positiveSamples: folder in tarPath containing positive samples
+    imagePath: directory within the tarfile to select and extract images
+    from, containing folders for positive and negative examples.
 
-    negativeSamples: folder in tarPath containing negative samples
+    positiveSamples: folder in imagePath containing positive samples
 
-    extractPath: directory to output the extracted tarfile images into
+    negativeSamples: folder in imagePath containing negative samples
 
-    trainPath: directory where the images selected for the training set 
-    are outputted
+    trainOutput: name of the file to output the training set to, without
+    any extensions or paths
 
-    testPath: directory where the images selected for the testing set 
-    are outputted
+    testOutput: name of the file to output the testing set to, without 
+    any extensions or paths
 
-    withReplacement: whether duplicate images are possible in the sets
+    withReplacement: whether or not duplicate images are possible in the
+    training and testing sets
 
-    randomState: NumPy seed for random selection reproducibility
+    randomSeed: NumPy seed for random selection reproducibility
 
 
-Default parameters are based on the project task statement
 Due to always selecting an equal number of positive and negative images 
 for the training set, the total number of images in the set will be 1 
 lower than trainSize when said parameter is odd
-The testing set will always have 100 positive and 100 negative images 
-per the project task statement
+The testing set will always have 100 positive and 100 negative images in
+order to follow the project task statement
 """
 def createDataset(
         tarfilePath, trainSize = 500, 
-        extractPath = './Data/extractedData', imagePath = '', 
+        workingPath = './WorkingData',  imagePath = '', 
         positiveSamples = 'positive', negativeSamples = 'negative',
-        trainPath = './Data/trainData', testPath = './Data/testData',
+        trainOutput = 'train_set', testOutput = 'test_set',
         withReplacement = False, randomSeed = None):
-    # Validate numeric parameters
+    # Validate parameters
     if not isinstance(trainSize, int) or trainSize < 1:
         raise ValueError(
             f'trainSize should be a positive integer, was {trainSize}'
         )
     if not isinstance(randomSeed, int):
         raise ValueError(f'randomSeed should be an integer, was {randomSeed}')
+    # Remove trailing slash from imagePath to prevent issues
+    imagePathClean = imagePath if imagePath[-1] != '/' else imagePath[:-1]
 
-    # Create output directories if they don't exist already
-    for path in [extractPath, trainPath, testPath]:
-        if not os.path.exists(path): os.makedirs(path)
+
+    # Create working directories, throw error if they already exist
+    try: os.mkdir(workingPath)
+    except FileExistsError: raise FileExistsError((
+        f'Specified working directory "{workingPath}" already exists; use a '
+        'unique directory name instead'
+    ))
+    (trainPath, testPath) = (
+        os.path.join(workingPath, trainOutput),
+        os.path.join(workingPath, testOutput),
+    )
+    for path in [trainPath, testPath]: os.mkdir(path)
 
     # Define possible image file extensions
     imageExts = {
@@ -98,106 +93,126 @@ def createDataset(
     # Load the dataset
     with tarfile.open(tarfilePath, 'r') as tf:
         # Check that the specified folders are in the tarfile
-        files = tf.getnames()
-        if len([item for item in files if item.startswith(imagePath)]) == 0:
+        fileNames = tf.getnames()
+        if len(
+            [item for item in fileNames if item.startswith(imagePathClean)]
+        ) == 0:
+            # Delete the working directory since about to throw error
+            shutil.rmtree(workingPath)
             raise FileNotFoundError((
-                f'Specified image directory "{imagePath}" not found in '
+                f'Specified image directory "{imagePathClean}" not found in '
                 f'tarfile "{tarfilePath}"'
             ))
-        elif os.path.join(imagePath, positiveSamples) not in files:
+        elif os.path.join(imagePathClean, positiveSamples) not in fileNames:
+            shutil.rmtree(workingPath)
             raise FileNotFoundError((
                 f'Specified positive sample directory "{positiveSamples}" not'
-                f' found in image directory "{imagePath}" within '
+                f' found in image directory "{imagePathClean}" within '
                 f'tarfile "{tarfilePath}"'
             ))
-        elif os.path.join(imagePath, negativeSamples) not in files:
+        elif os.path.join(imagePathClean, negativeSamples) not in fileNames:
+            shutil.rmtree(workingPath)
             raise FileNotFoundError((
                 f'Specified negative sample directory "{negativeSamples}" not'
-                f' found in image directory "{imagePath}" within '
+                f' found in image directory "{imagePathClean}" within '
                 f'tarfile "{tarfilePath}"'
             ))
         
-        # Extract just the images from the specified folder
+        # Get the images in the specified folder
+        files = []
         for file in tf.getmembers():
             filePath, fileExtension = os.path.splitext(file.name)
-            if filePath.startswith(imagePath + '/') and fileExtension in imageExts: 
-                tf.extract(file, path=extractPath)
+            if (filePath.startswith(imagePathClean + '/') and 
+                fileExtension in imageExts): 
+                files.append(file)
+
+        # Make sure enough samples were found for each type
+        positiveFiles = [
+            img for img in files if img.name.startswith(
+                os.path.join(imagePathClean, positiveSamples)
+            )
+        ]
+        negativeFiles = [
+            img for img in files if img.name.startswith(
+                os.path.join(imagePathClean, negativeSamples)
+            )
+        ]
+        if len(positiveFiles) == 0:
+            shutil.rmtree(workingPath)
+            raise FileNotFoundError((
+                'No positive samples were found in the tarfile. '
+                'Check that the specified directory '
+                f'"{os.path.join(imagePathClean, positiveSamples)}" is '
+                f'correct and that the specified tarfile "{tarfilePath}" '
+                'contains the desired images.'
+            ))
+        elif not withReplacement and len(positiveFiles) < (trainSize//2) + 100:
+            shutil.rmtree(workingPath)
+            raise ValueError((
+                f'Training and testing datasets require {(trainSize//2) + 100}'
+                f' positive samples total, but only {len(positiveFiles)} were '
+                f'found. Check that the tarfile "{tarfilePath}" contains '
+                'enough images, or set withReplacement = True to allow for '
+                'duplicate samples in the datasets.'
+            ))
+        elif len(negativeFiles) == 0:
+            shutil.rmtree(workingPath)
+            raise FileNotFoundError((
+                'No negative samples were found in the tarfile. '
+                'Check that the specified directory '
+                f'"{os.path.join(imagePathClean, negativeSamples)}" is '
+                f'correct and that the specified tarfile "{tarfilePath}" '
+                'contains the desired images.'
+            ))
+        elif not withReplacement and len(negativeFiles) < (trainSize//2) + 100:
+            shutil.rmtree(workingPath)
+            raise ValueError((
+                f'Training and testing datasets require {(trainSize//2) + 100}'
+                f' negative samples total, but only {len(negativeFiles)} were '
+                f'found. Check that the tarfile "{tarfilePath}" contains '
+                'enough images, or set withReplacement = True to allow for '
+                'duplicate samples in the datasets.'
+            ))
+        
+        # Set random state for reproducibility
+        rand = np.random.default_rng(randomSeed)
+        
+        # Randomly select enough files for training and testing sets
+        sampledPositive = [img for img in rand.choice(
+            positiveFiles, (trainSize//2) + 100, replace = withReplacement
+        )]
+        sampledNegative = [img for img in rand.choice(
+            negativeFiles, (trainSize//2) + 100, replace = withReplacement
+        )]
+
+        # Split out training and testing images to ensure there's no 
+        # overlap between the set images
+        testPositive = sampledPositive[:100]
+        trainPositive = sampledPositive[100:]
+        testNegative = sampledNegative[:100]
+        trainNegative = sampledNegative[100:]
+
+        # Extract the sampled images, using new names to avoid similarly
+        # named positive and negative samples overwriting each other
+        for index, img in enumerate(trainPositive + trainNegative):
+            _, ext = os.path.splitext(img.name)
+            tf._extract_member(
+                img, os.path.join(trainPath, (f'{index:05}' + ext))
+            )
+        for index, img in enumerate(testPositive + testNegative):
+            _, ext = os.path.splitext(img.name)
+            tf._extract_member(
+                img, os.path.join(testPath, (f'{index:05}' + ext))
+            ) 
     
-    # TODO: Check if sampling files from the tarfile without extracting 
-    # any other images is possible
+    # Compress the newly extracted datasets
+    with tarfile.open(trainOutput + '.tar.gz', "w:gz") as tar:
+        tar.add(trainPath, arcname = os.path.basename(trainPath))
+    with tarfile.open(testOutput + '.tar.gz', "w:gz") as tar:
+        tar.add(testPath, arcname = os.path.basename(testPath))
 
-    # Get locations of newly extracted positive and negative samples
-    extractedPositive = os.path.join(extractPath, imagePath, positiveSamples)
-    extractedNegative = os.path.join(extractPath, imagePath, negativeSamples)
-
-    # Make sure enough samples were extracted
-    positiveFiles = [
-        img for img in os.listdir(extractedPositive) if os.path.isfile(
-            os.path.join(extractPath, imagePath, positiveSamples, img)
-        )
-    ]
-    negativeFiles = [
-        img for img in os.listdir(extractedNegative) if os.path.isfile(
-            os.path.join(extractPath, imagePath, negativeSamples, img)
-        )
-    ]
-    if len(positiveFiles) == 0:
-        raise FileNotFoundError(
-            'No positive samples were extracted from the tarfile. Check'
-            f' that the specified directory "{extractedPositive}" is '
-            f'correct and that the specified tarfile "{tarfilePath}" '
-            'contains the desired images.'
-        )
-    elif not withReplacement and len(positiveFiles) < (trainSize//2) + 100:
-        raise ValueError(
-            f'Training and testing datasets require {(trainSize//2) + 100} '
-            f'positive samples total, but only {len(positiveFiles)} were '
-            f'found. Check that the tarfile "{tarfilePath}" contains enough '
-            'images, or set withReplacement = True to allow for duplicate '
-            'samples in the datasets.'
-        )
-    elif len(negativeFiles) == 0:
-        raise FileNotFoundError(
-            'No negative samples were extracted from the tarfile. Check'
-            f' that the specified directory "{extractedNegative}" is '
-            f'correct and that the specified tarfile "{tarfilePath}" '
-            'contains the desired images.'
-        )
-    elif not withReplacement and len(negativeFiles) < (trainSize//2) + 100:
-        raise ValueError(
-            f'Training and testing datasets require {(trainSize//2) + 100} '
-            f'negative samples total, but only {len(negativeFiles)} were '
-            f'found. Check that the tarfile "{tarfilePath}" contains enough '
-            'images, or set withReplacement = True to allow for duplicate '
-            'samples in the datasets.'
-        )
-    
-    # Set random state for reproducibility
-    rand = np.random.default_rng(randomSeed)
-
-    # Randomly select enough files for training and testing sets
-    sampledPositive = [img for img in rand.choice(
-        os.listdir(extractedPositive), (trainSize//2) + 100, 
-        replace = withReplacement
-    )]
-    sampledNegative = [img for img in rand.choice(
-        os.listdir(extractedNegative), (trainSize//2) + 100, 
-        replace = withReplacement
-    )]
-
-    # Split out training and testing images to ensure there's no overlap
-    testPositive, trainPositive = sampledPositive[:100], sampledPositive[100:]
-    testNegative, trainNegative = sampledNegative[:100], sampledNegative[100:]
-
-    # Copy selected images into their own directories, changing the file
-    # names to avoid similarly named positive and negative samples 
-    # overwriting each other
-    copyImageList(trainPositive, extractedPositive, 'p', trainPath)
-    copyImageList(trainNegative, extractedNegative, 'n', trainPath)
-    copyImageList(testPositive, extractedPositive, 'p', testPath)
-    copyImageList(testNegative, extractedNegative, 'n', testPath)
-    
-    # TODO: Compress extracted datasets 
+    # Delete the working directory now that sets have been compiled
+    shutil.rmtree(workingPath)
 
 # Run createDataset for testing
 createDataset(
